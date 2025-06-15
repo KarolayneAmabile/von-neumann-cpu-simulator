d39a118c3ae1de358b6f64dbf74aec6fe8005561
Karolayne <karolayneamabile@gmail.com>
2025-06-15 19:01:36 -0300

    faz a leitura de instruções que armazenam dados e trata erros

--- test.c
+++ test.c
@@ -26,28 +26,40 @@ void read(char *filename) {
 
     char str[100];
     char *tokens[5]; // posicaoNaMemoria;dadoOuInstrucao;palavra
+    char error[100]; // para exibir mensagem de erro
+    int line = 0;
 
-    while (fgets(str, sizeof(str), program) != NULL) { // line tem o conteudo de uma linha
-        // 0;i;ld r0, 96
+    while (fgets(str, sizeof(str), program) != NULL) { 
+        strcpy(error, str);
         int wordPosition = 0;
         char *token = strtok(str, "; ,\n");
-        // when we pass all the parameters of the string, this function
-        // will return only the *first* token of the string inpu
-        // after that, we *need to pass 'NULL'* insted of the input
-        // string to obtain the following toknes. it will return NULL
-        // when there is no token left
-        
+
         while (token) {
-            tokens[wordPosition] = token;
-            printf("%s", tokens[wordPosition]);
-            wordPosition++;
+            tokens[wordPosition++] = token;
             token = strtok(NULL, "; ,\n"); 
         }        
-        printf("\n");
+        // o array tokens possui cada pedaço da instrucao armazenado agora
+        // 0;i;ld r0, 96
+        // tokens[0] = posicao na memoria
+        // tokens[1] = se é dado ou instrucao
+        // para dado        tokens[2] = dado
+        // para instrucao   tokens[2] = opcode
+        if (strcmp(tokens[1], "d") == 0) { // yep, funciona perfeitamente pra dados
+            int memoryAddress = (int)strtol(tokens[0], NULL, 16);
+            int data = (int)strtol(tokens[2], NULL, 16);
+
+            memory[memoryAddress] = data >> 8;
+            memory[++memoryAddress] = data & 0xff;
+        } else if (strcmp(tokens[1], "i") == 0) {
+            // do
+        } else {
+            //"Erro: tipo inválido \"%s\". Apenas \"i\" (instrução) ou \"d\" (dado) são permitidos.\n", tokens[1])
+            printf("Syntax error on line: %i - %s", line, error);
+            printf("Invalid type \"%s\". Only \"i\" (instruction) or \"d\" (data) are allowed.\n", tokens[1]);
+            return;
+        }
+
+        line++;
     }
     fclose(program);
 }
-
-// char* strtok(char *str, const char *delimiter)
-// str: Original String that is to be processed
-// delimiter: The separator using which we tokenize the string.
