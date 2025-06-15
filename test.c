dc23748a8843b90d25a3e826c33add7ee70c0383
Karolayne <karolayneamabile@gmail.com>
2025-06-15 17:57:55 -0300

    a funcao e capaz de separar palavras em tokens corretamente

--- test.c
+++ test.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void read(char *filename);
+
+unsigned char memory[154] = {0};
 
 // argc: will be the count of input arguments to your program
 // argv: will be a pointer to all the input arguments
@@ -22,11 +25,29 @@ void read(char *filename) {
     }
 
     char str[100];
-    int line = 0;
+    char *tokens[5]; // posicaoNaMemoria;dadoOuInstrucao;palavra
 
-    while (fgets(str, sizeof(str), program) != NULL) {
-        printf("Line: %d: %s", line, str);
-        line++;
+    while (fgets(str, sizeof(str), program) != NULL) { // line tem o conteudo de uma linha
+        // 0;i;ld r0, 96
+        int wordPosition = 0;
+        char *token = strtok(str, "; ,\n");
+        // when we pass all the parameters of the string, this function
+        // will return only the *first* token of the string inpu
+        // after that, we *need to pass 'NULL'* insted of the input
+        // string to obtain the following toknes. it will return NULL
+        // when there is no token left
+        
+        while (token) {
+            tokens[wordPosition] = token;
+            printf("%s", tokens[wordPosition]);
+            wordPosition++;
+            token = strtok(NULL, "; ,\n"); 
+        }        
+        printf("\n");
     }
     fclose(program);
 }
+
+// char* strtok(char *str, const char *delimiter)
+// str: Original String that is to be processed
+// delimiter: The separator using which we tokenize the string.
