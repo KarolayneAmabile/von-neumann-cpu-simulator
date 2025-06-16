443a8c37a3686e7dae3e3438f2393e3ff6894741
Karolayne <karolayneamabile@gmail.com>
2025-06-16 20:43:48 -0300

    add getOpcode func

---
+++ a.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int word = 2 << 2;
+    printf("%X", word);
+}
--- test.c
+++ test.c
@@ -28,6 +28,8 @@ void read(char *filename) {
     char *tokens[5]; // posicaoNaMemoria;dadoOuInstrucao;palavra
     char error[100]; // para exibir mensagem de erro
     int line = 0;
+    int memoryAddress = 0;
+    int word = 0;
 
     while (fgets(str, sizeof(str), program) != NULL) { 
         strcpy(error, str);
@@ -38,6 +40,8 @@ void read(char *filename) {
             tokens[wordPosition++] = token;
             token = strtok(NULL, "; ,\n"); 
         }        
+
+        memoryAddress = (int)strtol(tokens[0], NULL, 16);
         // o array tokens possui cada pedaço da instrucao armazenado agora
         // 0;i;ld r0, 96
         // tokens[0] = posicao na memoria
@@ -45,13 +49,89 @@ void read(char *filename) {
         // para dado        tokens[2] = dado
         // para instrucao   tokens[2] = opcode
         if (strcmp(tokens[1], "d") == 0) { // yep, funciona perfeitamente pra dados
-            int memoryAddress = (int)strtol(tokens[0], NULL, 16);
             int data = (int)strtol(tokens[2], NULL, 16);
 
             memory[memoryAddress] = data >> 8;
             memory[++memoryAddress] = data & 0xff;
+            //
+            //
+            //
         } else if (strcmp(tokens[1], "i") == 0) {
-            // do
+            if (strcmp(tokens[2], "hlt") == 0) { 
+                memory[memoryAddress] = 0;
+            } else if (strcmp(tokens[2], "nop") == 0) {
+                memory[memoryAddress] = 1;
+
+
+
+            } else if (strcmp(tokens[2], "ldr") == 0) {
+                word = 2 << 2;
+                word = (word | atoi(tokens[3] + 1)) << 2;
+                word = (word | atoi(tokens[4] + 1)) << 7;
+                memory[memoryAddress] = word >> 8;
+                memory[++memoryAddress] = word & 0xff;
+
+            } else if (strcmp(tokens[2], "str") == 0) {
+                word = 3 << 2;
+            } else if (strcmp(tokens[2], "add") == 0) {
+                word = 4 << 2;
+            } else if (strcmp(tokens[2], "sub") == 0) {
+                word = 5 << 2;
+            } else if (strcmp(tokens[2], "mul") == 0) {
+                word = 6 << 2;
+            } else if (strcmp(tokens[2], "div") == 0) {
+                word = 7 << 2;
+            } else if (strcmp(tokens[2], "cmp") == 0) {
+                word = 8 << 2;
+            } else if (strcmp(tokens[2], "movrr") == 0) {
+                word = 9 << 2;
+            } else if (strcmp(tokens[2], "and") == 0) {
+                word = 10 << 2;
+            } else if (strcmp(tokens[2], "or") == 0) {
+                word = 11 << 2;
+            } else if (strcmp(tokens[2], "xor") == 0) {
+                word = 12 << 2;
+            
+
+
+
+
+                
+            } else if (strcmp(tokens[2], "nop") == 0) {
+                
+            } else if (strcmp(tokens[2], "div") == 0) {
+                
+            } else if (strcmp(tokens[2], "div") == 0) {
+                
+            } else if (strcmp(tokens[2], "div") == 0) {
+                
+            } else if (strcmp(tokens[2], "div") == 0) {
+                
+            } else if (strcmp(tokens[2], "div") == 0) {
+                
+            } else if (strcmp(tokens[2], "div") == 0) {
+                
+            } else if (strcmp(tokens[2], "div") == 0) {
+                
+            } else if (strcmp(tokens[2], "div") == 0) {
+                
+            } else if (strcmp(tokens[2], "div") == 0) {
+                
+            } else if (strcmp(tokens[2], "div") == 0) {
+                
+            } else if (strcmp(tokens[2], "div") == 0) {
+                
+            } else if (strcmp(tokens[2], "div") == 0) {
+                
+            } else if (strcmp(tokens[2], "div") == 0) {
+                
+            } else {
+                //
+            }
+        //
+        //
+        //
+        //
         } else {
             //"Erro: tipo inválido \"%s\". Apenas \"i\" (instrução) ou \"d\" (dado) são permitidos.\n", tokens[1])
             printf("Syntax error on line: %i - %s", line, error);
@@ -62,4 +142,42 @@ void read(char *filename) {
         line++;
     }
     fclose(program);
+}
+
+int getOpcode(char *mnemonic) {
+    if (strcmp(mnemonic, "hlt") == 0) return 0;
+    if (strcmp(mnemonic, "nop") == 0) return 1;
+
+    if (strcmp(mnemonic, "ldr") == 0) return 2;
+    if (strcmp(mnemonic, "str") == 0) return 3;
+    if (strcmp(mnemonic, "add") == 0) return 4;
+    if (strcmp(mnemonic, "sub") == 0) return 5;
+    if (strcmp(mnemonic, "mul") == 0) return 6;
+    if (strcmp(mnemonic, "div") == 0) return 7;
+    if (strcmp(mnemonic, "cmp") == 0) return 8;
+    if (strcmp(mnemonic, "movrr") == 0) return 9;
+    if (strcmp(mnemonic, "and") == 0) return 10;
+    if (strcmp(mnemonic, "or") == 0) return 11;
+    if (strcmp(mnemonic, "xor") == 0) return 12;
+
+    if (strcmp(mnemonic, "not") == 0) return 13;
+
+    if (strcmp(mnemonic, "je") == 0) return 14;
+    if (strcmp(mnemonic, "jne") == 0) return 15;
+    if (strcmp(mnemonic, "jl") == 0) return 16;
+    if (strcmp(mnemonic, "jle") == 0) return 17;
+    if (strcmp(mnemonic, "jg") == 0) return 18;
+    if (strcmp(mnemonic, "jge") == 0) return 19;
+    if (strcmp(mnemonic, "jmp") == 0) return 20;
+
+    if (strcmp(mnemonic, "ld") == 0) return 21;
+    if (strcmp(mnemonic, "st") == 0) return 22;
+
+    if (strcmp(mnemonic, "movi") == 0) return 23;
+    if (strcmp(mnemonic, "addi") == 0) return 24;
+    if (strcmp(mnemonic, "subi") == 0) return 25;
+    if (strcmp(mnemonic, "muli") == 0) return 26;
+    if (strcmp(mnemonic, "divi") == 0) return 27;
+    if (strcmp(mnemonic, "lsh") == 0) return 28;
+    if (strcmp(mnemonic, "rsh") == 0) return 29;
 }
